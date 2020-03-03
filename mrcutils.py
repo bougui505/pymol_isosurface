1cc5f1df92463f0520b257c5f28e1e5a8523c0c5
Guillaume Bouvier <bougui@mantrisse.bis.pasteur.fr>
2020-03-03 10:38:34 +0100

    Crop added in MRC class 	modified:   mrcutils.py

--- mrcutils.py
+++ mrcutils.py
@@ -10,30 +10,6 @@ import numpy
 import scipy.spatial.distance
 
 
-def read_mrc(mrcfilename):
-    """
-    Read a mrc file and return the xyz and density values at the given level
-    if given
-    """
-    xyz = []
-    with mrcfile.open(mrcfilename) as emd:
-        nx, ny, nz = emd.header['nx'], emd.header['ny'], emd.header['nz']
-        x0, y0, z0 = emd.header['origin']['x'], emd.header['origin']['y'],\
-            emd.header['origin']['z']
-        dx, dy, dz = emd.voxel_size['x'], emd.voxel_size['y'], emd.voxel_size['z']
-        xyz = numpy.meshgrid(numpy.arange(x0, x0 + nx * dx, dx),
-                             numpy.arange(y0, y0 + ny * dy, dy),
-                             numpy.arange(z0, z0 + nz * dz, dz),
-                             indexing='ij')
-        xyz = numpy.asarray(xyz)
-        xyz = xyz.reshape(3, nx * ny * nz)
-        xyz = xyz.T
-        return xyz.reshape(nx, ny, nz, 3),
-        emd.data.flatten(order='F').reshape(nx, ny, nz)
-
-
-
-
 def first_to_last(arr):
     """
     Put first axis of an array to last
@@ -63,27 +39,6 @@ def zone(mrc, coords, radius, mrcfilename):
     return mrc_zone
 
 
-def crop(mrc, coords, padding, mrcfilename):
-    """
-    Get a new mrc object cropping around the given coordinates with the given
-    padding
-    """
-    xyz_min = coords.min(axis=0)
-    xyz_max = coords.max(axis=0)
-    grid_coords = mrc.grid_coords.reshape((-1, 3))
-    grid_indices = mrc.grid_indices.reshape((-1, 3))
-    ind_min = numpy.linalg.norm(grid_coords - xyz_min, axis=1).argmin()
-    ind_max = numpy.linalg.norm(grid_coords - xyz_max, axis=1).argmin()
-    ind_min = grid_indices[ind_min]
-    ind_max = grid_indices[ind_max]
-    grid_crop = mrc.grid[ind_min[0]:ind_max[0],
-                         ind_min[1]:ind_max[1],
-                         ind_min[2]:ind_max[2]]
-    origin_crop = mrc.grid_coords[tuple(ind_min)]
-    save_density(density=grid_crop, outfilename=mrcfilename, spacing=mrc.step,
-                 origin=origin_crop)
-    mrc_crop = MRC(mrcfilename)
-    return mrc_crop
 
 
 class MRC(object):
@@ -91,11 +46,11 @@ class MRC(object):
         self.mrcfilename = mrcfilename
         self.read_mrc(self.mrcfilename)
 
-    def read_mrc(self, mrcfilename):
-        with mrcfile.open(mrcfilename) as emd:
+    def read_mrc(self, mrcfilename=None):
+        if mrcfilename is not None:
+            self.mrcfilename = mrcfilename
+        with mrcfile.open(self.mrcfilename) as emd:
             nx, ny, nz = emd.header['nx'], emd.header['ny'], emd.header['nz']
-            # print('Reading density:')
-            # print(tuple(numpy.int_([nx, ny, nz])))
             x0, y0, z0 = emd.header['origin']['x'], emd.header['origin']['y'],\
                 emd.header['origin']['z']
             dx, dy, dz = emd.voxel_size['x'], emd.voxel_size['y'], emd.voxel_size['z']
@@ -137,3 +92,23 @@ class MRC(object):
             nx, ny, nz = mrc.header['nx'], mrc.header['ny'], mrc.header['nz']
             print('Writing density:')
             print(tuple(numpy.int_([nx, ny, nz])))
+
+    def crop(self, coords, padding, mrcfilename=None):
+        """
+        Get a new mrc object cropping around the given coordinates with the given
+        padding
+        """
+        xyz_min = coords.min(axis=0)
+        xyz_max = coords.max(axis=0)
+        grid_coords = self.grid_coords.reshape((-1, 3))
+        grid_indices = self.grid_indices.reshape((-1, 3))
+        ind_min = numpy.linalg.norm(grid_coords - xyz_min, axis=1).argmin()
+        ind_max = numpy.linalg.norm(grid_coords - xyz_max, axis=1).argmin()
+        ind_min = grid_indices[ind_min]
+        ind_max = grid_indices[ind_max]
+        self.grid = self.grid[ind_min[0]:ind_max[0],
+                              ind_min[1]:ind_max[1],
+                              ind_min[2]:ind_max[2]]
+        self.origin = self.grid_coords[tuple(ind_min)]
+        self.write_mrc(mrcfilename=mrcfilename)
+        self.read_mrc(mrcfilename=mrcfilename)
